77f659964c86511d48b0092398013a50a138d169
seungCheolCha <cscculture95@gmail.com>
2021-02-25 11:01:13 +0900

    초기 커밋.

---
+++ First_Web_App/Calc.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" 
+    AutoEventWireup="true" 
+    CodeBehind="Calc.aspx.cs" 
+    Inherits="First_Web_App.Calc" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Height="18px" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+        </div>
+    </form>
+</body>
+</html>
---
+++ First_Web_App/Calc.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace First_Web_App
+{
+    public partial class Calc : System.Web.UI.Page
+    {
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int number = Int32.Parse(TextBox1.Text);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
+            {
+                sb.AppendFormat("{0} * {1} = {2}<br/>", number, i, number * i);
+            }
+            Literal1.Text = sb.ToString();
+        }
+    }
+}
